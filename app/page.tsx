2a0e58114ec5657221c7a4e9b5fcd1b716353be5
Patience Emaiku <114579543+CallMePat@users.noreply.github.com>
2024-04-18 00:46:31 +0100

    Feature/frontend UI (#6)
    * Merge Developmrnt branch
    
    * Edited the PropertyListing component
    
    * Editted the Property Listing component and also the detailed page for listing
    
    * Worked on the Landing page and Faq page
    
    ---------
    
    Co-authored-by: PrimeReservedDevelopers <166627959+PrimeReservedDevelopers@users.noreply.github.com>
    Co-authored-by: Joshua-Chris <joshuadgreat04@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Hero from './components/Hero';
-
-// import BlogContent from './components/BlogContent/BlogContent';
-import { client } from './lib/createClient';
-import { groq } from 'next-sanity';
+import Hero from "./components/Hero";
+import AboutProperty from "./components/LandingPage/AboutProperty";
+import BlogHomePage from "./components/LandingPage/BlogHomePage";
+import Card from "./components/LandingPage/Card";
+import Review from "./components/LandingPage/Review";
+import Newsletter from "./components/NewsLetter/Newletter";
+import { client } from "./lib/createClient";
+import { groq } from "next-sanity";
 
 export const revalidate = 30;
 const query = groq`*[_type == 'post']{
@@ -17,7 +20,39 @@ export default async function Home() {
   return (
     <main>
       <Hero />
-      {/* <BlogContent posts={posts} /> */}
+
+      {/* Discover Property */}
+      <div className=" wrapper flex justify-center my-[6rem] ">
+        <div className="flex flex-col xl:w-[40%] md:mx-20 mx-32 text-center ">
+          <h1 className="text-2xl font-medium">
+            Effortless Property Dicovery, Just For You
+          </h1>
+          <p className="text-sm font-medium my-5 text-customTextColor leading-loose">
+            Dive into a realm where walls and roofs transform into the backdrop
+            of your unique story. Your next home is not just a space , it a
+            canvas inviting you to paint the chapters of your life
+          </p>
+
+          <div className="flex justify-center">
+            <div className="flex gap-5 justify-center bg-white drop-shadow-lg py-10 w-[400px] ">
+              <button className="py-3 px-[3.5rem] border-[1px] border-primary rounded-md text-xs text-primary ">
+                Land
+              </button>
+              <button className="py-3 px-[3rem] bg-primary rounded-md text-xs text-white">
+                Smart Homes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Land */}
+      <Card />
+      <AboutProperty />
+
+      <BlogHomePage posts={posts} />
+      <Review />
+      <Newsletter />
     </main>
   );
 }
