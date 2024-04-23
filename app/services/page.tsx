67b96c7b7846e307f044ce6eb6627b6defa455b5
CallMePat <patienceemaiku@gmail.com>
2024-04-23 14:33:27 +0100

    Adjust Service and About page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React from "react";
+import ContactUs from "../components/Contact/ContactUs";
+import Service from "../components/Services/Services";
+import CustomerTestimony from "../components/Services/CustomerTestimony";
 
 const ServicesPage = () => {
   return (
-    <section className="relative w-full overflow-hidden bg-center bg-cover mt-20 md:mt-[7.3rem] py-20 md:py-28">
-      Services Page
-    </section>
+    <main className="pt-[6.46rem] md:pt-[7.3rem]">
+      {/* Hero section */}
+      <div
+        className="relative flex h-96 items-center justify-center bg-cover bg-center px-4"
+        style={{
+          backgroundImage: "url('/images/services/Hero Section.svg')",
+        }}
+      ></div>
+
+      <Service />
+      <CustomerTestimony />
+      <ContactUs />
+    </main>
   );
 };
 
